4797d131f2f97345c8aa74c74009a5a69c9a5bca
Amalya <amalya.fatullaeva97@gmail.com>
2024-06-30 02:38:31 +0700

    Добавлен функционал класса Chat

---
+++ Chat.cpp
@@ -0,0 +1,134 @@
+#include "Chat.h"
+
+Chat::Chat(int n)
+{
+	_maxcount = n;
+	_allUsers = new Users[_maxcount]{};
+}
+
+Chat::~Chat()
+{
+	delete[]_allUsers;
+}
+
+void Chat::registration()
+{
+	char c = 'y';
+	int i;
+	if (_count < _maxcount)
+	{
+		while (c != 'n')
+		{
+			std::cout << "Регистрация нового пользователя\n";
+			_allUsers[_count].setUser();
+			for (i = 0; i < _count; i++)
+			{
+				if (_allUsers[_count].getLogin() == _allUsers[i].getLogin())
+				{
+					std::cout << "Пользователь с таким логином уже существует!\nХотите повторить попытку?(y/n)";
+					std::cin >> c;
+					break;
+				}
+			}
+
+			if (i == _count)
+			{
+				_count++;
+				c = 'n';
+			}
+		}
+		
+	}
+	else
+	{
+		std::cout << "Зарегистрировано максимальное кол-во пользователей!\n";
+	}
+	
+}
+
+void Chat::getChat()
+{
+	for (int i = 0; i < _maxcount; i++)
+	{
+		std::cout << _allUsers[i];
+	}
+}
+
+void Chat::enter()
+{
+	char c = 'y';
+	int i;
+	while (c != 'n')
+	{
+		std::cout << "Для входа введите логин: \n";
+		std::cin >> _login;
+		for (i = 0; i < _maxcount; i++)
+		{
+			if (_login == _allUsers[i].getLogin())
+			{
+				break;
+			}
+		}
+		if (i == _maxcount)
+		{
+			std::cout << "Пользователь не найден!\nХотите повторить ввод?(y/n): ";
+			std::cin >> c;
+		}
+		else
+		{
+			std::cout << "Введите пароль: \n";
+			std::cin >> _password;
+
+			if (_password != _allUsers[i].getPassword())
+			{
+				std::cout << "Неправильный пароль!\nХотите повторить ввод?(y/n): ";
+				std::cin >> c;
+			}
+			else
+			{
+				c = 'n';
+				if (_login == _recipient)
+				{
+					std::cout << "У вас есть новое сообщение: ";
+					_privateMessage->getMessage();
+				}
+					
+			}
+		}
+	}
+	
+}
+
+
+void Chat::sendPrivateMessage()
+{
+	char c = 'y';
+	int i;
+	_sender = _login;
+	while (c != 'n')
+	{
+		std::cout << "Кому: ";
+		std::cin >> _recipient;
+		for (i = 0; i < _count; i++)
+		{
+			if (_recipient == _allUsers[i].getLogin())
+			{
+				break;
+			}
+		}
+		if (i == _count)
+		{
+			std::cout << "Получаетль не найден!\nХотите повторить попытку?(y/n)";
+			std::cin >> c;
+		}
+		else
+		{
+			std::cout << "ВВедите сообщение:\n";
+			_privateMessage = new Message;
+			_privateMessage->setMessage();
+			c = 'n';
+		}
+	}
+	
+	
+}
